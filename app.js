da707829f7c642a9b7928a0081d0eb3575b74979
Bugra Aydingoz <bugraaydingoz@gmail.com>
2018-06-08 16:36:29 +0300

    Heroku update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-//const io = require('socket.io')()
-const io = socketIO(app)
+const io = require('socket.io')(app)
 const path = require('path')
 const Stock = require('./controllers/Stock')
 
